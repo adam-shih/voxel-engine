6def0c06e6292372d53e40d7743d20ed0b13df54
Adam <shihadam1@gmail.com>
2023-04-05 17:35:52 -0700

    Fixed lighting by computing normals

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,8 @@ fn setup(
     let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
     mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
     mesh.set_indices(Some(Indices::U32(indices)));
+    mesh.duplicate_vertices();
+    mesh.compute_flat_normals();
     commands.spawn(PbrBundle {
         mesh: meshes.add(mesh),
         material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
